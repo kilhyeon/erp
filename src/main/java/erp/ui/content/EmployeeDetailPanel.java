c626b8882d3bc703d8753617d7d3a83c6058f7ec
kilhyeon <tizona@naver.com>
2021-03-16 11:14:21 +0900

    사원 상세정보

---
+++ src/main/java/erp/ui/content/EmployeeDetailPanel.java
@@ -0,0 +1,38 @@
+package erp.ui.content;
+
+import erp.dto.EmployeeDetail;
+
+public class EmployeeDetailPanel extends AbstractContentPanel<EmployeeDetail> {
+
+	/**
+	 * Create the panel.
+	 */
+	public EmployeeDetailPanel() {
+
+	}
+
+	@Override
+	public void setItem(EmployeeDetail item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public EmployeeDetail getItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void validChek() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clearTf() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
